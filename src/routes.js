60363a20b63c078fd5e62beb0b0c94bf8f6712e2
Fernando Rodrigues <fernandorfigueiredotec@gmail.com>
2020-06-18 04:26:26 -0300

    Test and procfile

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
-const { Router } = require('express');
+const { Router, request, response } = require('express');
 const emprestimocontroller = require('./controllers/emprestimocontroller');
 
 const routes = Router();
 
 routes.post('/',emprestimocontroller.create);
 routes.post('/simular',emprestimocontroller.simular);
-
+routes.get('/test',(request,response) => {
+    return response.json('We did it')
+})
 module.exports = routes;
